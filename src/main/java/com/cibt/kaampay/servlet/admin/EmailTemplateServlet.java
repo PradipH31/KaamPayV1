a857b5b0cca6ec9f9c401d0466548f7ed3cdc6d3
PradipH31 <pradip.hamal31@gmail.com>
2019-06-01 14:03:32 +0545

    EmailTemplateServlet extend HttpServlet index for emailtemplate

--- src/main/java/com/cibt/kaampay/servlet/admin/EmailTemplateServlet.java
+++ src/main/java/com/cibt/kaampay/servlet/admin/EmailTemplateServlet.java
@@ -5,10 +5,28 @@
  */
 package com.cibt.kaampay.servlet.admin;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author HP B&O
  */
-public class EmailTemplateServlet {
-    
+@WebServlet(urlPatterns = "/admin/emailtemplates/*")
+public class EmailTemplateServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/views/admin/emailtemplates/index.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/views/admin/emailtemplates/index.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : index
+    Created on : Jun 1, 2019, 2:00:57 PM
+    Author     : HP B&O
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Email Templates</h1>
+        <hr>    
+        <p>
+            <a href="/admin/emailtemplates/add">Add</a>
+        </p>
+    </body>
+</html>
