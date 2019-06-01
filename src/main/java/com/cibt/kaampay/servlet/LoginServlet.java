ffacb4562ba8546d7d6fc4293ff3865efde1270b
PradipH31 <pradip.hamal31@gmail.com>
2019-06-01 09:21:08 +0545

    Added the get and post methods for the servlet

--- src/main/java/com/cibt/kaampay/servlet/LoginServlet.java
+++ src/main/java/com/cibt/kaampay/servlet/LoginServlet.java
@@ -5,12 +5,26 @@
  */
 package com.cibt.kaampay.servlet;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author HP B&O
  */
-public class LoginServlet extends HttpServlet{
-    
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
