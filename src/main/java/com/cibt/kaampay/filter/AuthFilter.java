c3a4da4198b9cdab3be237957fd341325db08c23
PradipH31 <pradip.hamal31@gmail.com>
2019-06-01 14:13:45 +0545

    AuthFilter implements Filter Getting session,request,response and user from the session

--- src/main/java/com/cibt/kaampay/filter/AuthFilter.java
+++ src/main/java/com/cibt/kaampay/filter/AuthFilter.java
@@ -5,10 +5,36 @@
  */
 package com.cibt.kaampay.filter;
 
+import com.cibt.kaampay.entity.User;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author HP B&O
  */
-public class AuthFilter {
+public class AuthFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false);
+        if (session != null && session.getAttribute("loggedin") != null) {
+            User user = (User) session.getAttribute("loggedin");
+            if (user != null && user.isStatus()) {
+                fc.doFilter(request, response);
+            } else {
+                resp.sendRedirect(req.getContextPath() + "/login");
+            }
+        }
+    }
     
 }
